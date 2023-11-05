edbc0f813090763365de6f788c99b49276cb9991
Sayem98 <sayemabedin.bd@gmail.coom>
2023-11-06 01:47:29 +0600

    [structure]: project structure is created.

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div>
+        <p className="text-3xl text-center">AES Blockchain Explorer</p>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
