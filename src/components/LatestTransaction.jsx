b8e2d4f27eae7507b929064ce1979c0c880f9fca
Sayem98 <sayemabedin.bd@gmail.coom>
2023-11-06 23:21:58 +0600

    Responsive design

--- src/components/LatestTransaction.jsx
+++ src/components/LatestTransaction.jsx
@@ -2,7 +2,7 @@ import { BiCube } from "react-icons/bi";
 
 function SingleItem({ number, type, time, fee, recipient, txns, reward }) {
   return (
-    <div className="w-full flex items-center justify-between px-4 py-4 border-b border-gray-200">
+    <div className="w-full flex flex-col gap-2 md:gap-0 md:flex-row md:items-center justify-between px-4 py-4 border-b border-gray-200">
       <div className="flex items-center gap-2">
         <div className="bg-[#f8f9fa] p-3 rounded-lg">
           <BiCube size={20} />
@@ -38,7 +38,7 @@ function SingleItem({ number, type, time, fee, recipient, txns, reward }) {
         )}
       </div>
 
-      <span className="border text-sm border-gray-300 p-1 rounded-xl">
+      <span className="border self-start text-sm border-gray-300 p-1 rounded-xl">
         {reward} Eth
       </span>
     </div>
@@ -48,7 +48,7 @@ function SingleItem({ number, type, time, fee, recipient, txns, reward }) {
 function LatestTransaction() {
   return (
     <div className="max-w-[1400px] w-full mx-auto px-6 space-y-3 mt-6">
-      <div className="flex items-start gap-6">
+      <div className="flex flex-col md:flex-row items-start gap-6">
         <div className="bg-white border border-gray-200 rounded-lg w-full">
           <h2 className="border-b pb-4 font-semibold py-2 px-4">
             Latest Blocks
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,13 +2,13 @@ import { FaSearch } from "react-icons/fa";
 
 function SearchBar() {
   return (
-    <div className="w-full h-[200px] bg-[#1f2937] px-6 py-8">
+    <div className="w-full h-[200px] bg-[#1f2937] md:px-6 py-8">
       <div className="max-w-[1400px] w-full mx-auto px-6 space-y-3">
         <h2 className="text-lg text-white font-semibold">
           AES Blockchain Explorer
         </h2>
-        <div className="flex items-center gap-2 bg-white px-4 py-2 rounded-lg w-[70%]">
-          <select className="focus:outline-none w-[7rem]">
+        <div className="flex items-center gap-2 bg-white px-4 py-2 rounded-lg md:w-[70%]">
+          <select className="focus:outline-none md:w-[7rem]">
             <option value="address" className="text-semibold">
               Address
             </option>
